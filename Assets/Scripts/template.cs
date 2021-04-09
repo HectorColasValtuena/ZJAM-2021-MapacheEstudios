807f6b4f655332b12e45b6a0d730b3aa1344a2d1
HectorColasValtuena <MephistoII666@gmail.com>
2021-04-09 22:58:20 +0200

    filler files update

---
+++ Assets/Scripts/template.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class template : MonoBehaviour
+{
+//Interface implementation
+//ENDOF Interface implementation
+
+//protected hierarchy values
+
+
+//ENDOF protected hierarchy values
+
+//private variables
+//ENDOF private variables
+
+
+//MonoBehaviour lifecycle
+//ENDOF MonoBehaviour lifecycle
+
+//public methods
+//ENDOF public methods
+
+//private methods
+//ENDOF private methods 
+}
