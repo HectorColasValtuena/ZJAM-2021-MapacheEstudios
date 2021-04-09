9a9d91e1e100c013c4903d9579f18ed7e2e3e48d
HectorColasValtuena <MephistoII666@gmail.com>
2021-04-09 20:36:05 +0200

    Input Controller

---
+++ Assets/Scripts/InputController/IInputController.cs
@@ -0,0 +1,7 @@
+﻿using Vector2 = UnityEngine.Vector2;
+
+public interface IInputController
+{
+    Vector2 direction {get;}
+    bool dumpWater {get;}
+}
---
+++ Assets/Scripts/InputController/InputController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class InputController : IInputController
+{
+    public static IInputController instance
+    {
+    	get {
+    		if (_instance == null)
+    		{
+    			_instance = new InputController();
+    		}
+    		return _instance;
+    	}
+    }
+    private static IInputController _instance;
+
+//IInputController implementation
+	public Vector2 direction
+	{
+		get
+		{
+			return new Vector2(
+				Input.GetAxis("Horizontal"),
+				Input.GetAxis("Vertical")
+			);
+		}
+	}
+
+	public bool dumpWater
+	{
+		get
+		{
+			return Input.GetKey(KeyCode.Space);
+		}
+	}
+//ENDOF IInputController implementation
+}
