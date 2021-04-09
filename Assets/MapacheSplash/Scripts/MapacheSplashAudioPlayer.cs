ec1dbb5f5e1f895e64d3fcc5863df8a63a5d0f24
HectorColasValtuena <MephistoII666@gmail.com>
2021-04-09 20:35:42 +0200

    added mapache splash

---
+++ Assets/MapacheSplash/Scripts/MapacheSplashAudioPlayer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace MapacheSplash
+{
+	public class MapacheSplashAudioPlayer : MonoBehaviour
+	{
+		[SerializeField]
+		public AudioSource[] audioSources;
+
+		public void PlayAll ()
+		{
+			foreach (AudioSource audioSource in audioSources)
+			{
+				Play(audioSource);
+			}
+		}
+
+		public void PlayByIndex (int index)
+		{
+			Play(audioSources[index]);
+		}
+
+		private void Play (AudioSource audioSource)
+		{
+			audioSource.Play();
+		}
+	}
+}
---
+++ Assets/MapacheSplash/Scripts/MapacheSplashSceneChanger.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using SceneManager = UnityEngine.SceneManagement.SceneManager;
+
+namespace MapacheSplash
+{
+	public class MapacheSplashSceneChanger : MonoBehaviour
+	{
+		[SerializeField]
+		private int nextScene = 1;
+
+		public void SplashFinishedSceneChange ()
+		{
+			SceneManager.LoadScene(nextScene);
+		}
+	}
+}
