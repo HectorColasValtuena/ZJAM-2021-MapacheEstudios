96baef3e6d29ea814bd02b505a3b5229493f556b
HectorColasValtuena <MephistoII666@gmail.com>
2021-04-10 07:17:03 +0200

    salpicadurita

---
+++ Assets/Scripts/SpecialBuildings/WaterTowerController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterTowerController : MonoBehaviour
+{
+//Constants
+	private const float reloadTime = 240f;
+//ENDOF Constants
+
+//static space
+	private bool freeReload { get { return false; }}
+//ENDOF static space
+
+//MonoBehaviour lifecycle
+//ENDOF MonoBehaviour lifecycle
+}
