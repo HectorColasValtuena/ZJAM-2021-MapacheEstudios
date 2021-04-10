e2bb17a0185ff13f840f672c4bdeaa7f63f49fd0
HectorColasValtuena <MephistoII666@gmail.com>
2021-04-10 06:04:53 +0200

    Particle billboarding

---
+++ Assets/Scripts/EffectControllers/SpriteLookAtCamera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteLookAtCamera : MonoBehaviour
+{
+    public void Update ()
+    {
+    	/*
+    	transform.LookAt(
+    		target: Camera.main.transform,
+    		worldUp: Vector3.back
+    	);
+    	//*/
+    	transform.forward = Camera.main.transform.forward;
+    }
+}
