2c7f8f91f895dfda29c3ab4cfa154973e58c87d7
HectorColasValtuena <MephistoII666@gmail.com>
2021-04-11 08:23:58 +0200

    resolucion y quit

---
+++ Assets/Scripts/ScreenResolution.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenResolution : MonoBehaviour
+{
+    public void Awake ()
+    {
+    	Debug.LogError("ScreenResolution");
+    	Screen.SetResolution(320, 240, true);
+    }
+}
